8744542e6cd9d06611dd5590dc74f62de90c21bf
LuisMirandaMallqui <a20223796@pucp.edu.pe>
2025-08-22 01:49:51 -0500

    Primera version - Postulantes

---
+++ LAB1/LAB2_2025_1_Miranda/LAB2_2025_1_Miranda/Postulante.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2_2025_1_Miranda
+{
+    public class Postulante
+    {
+
+    }
+}
