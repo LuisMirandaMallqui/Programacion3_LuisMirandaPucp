d2862a7593ba1cd9b5ca22cfa1b7026e8f5a022f
LuisMirandaMallqui <a20223796@pucp.edu.pe>
2025-08-22 01:05:30 -0500

    Commit de Testeo
    Probando

---
+++ LAB1/LAB2_2025_1_Miranda/LAB2_2025_1_Miranda/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2_2025_1_Miranda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
