813fa9f442aad774ae463acd895cefa7b529d603
LuisMiranda <alulab14@V20208>
2025-08-25 11:50:09 -0500

    Avance de los primeros 3 incisos del enunciadasdasdas

---
+++ LAB1/LAB2_2025_1_Miranda/LAB2_2025_1_Miranda/Admision.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2_2025_1_Miranda
+{
+    class Admision
+    {
+        private List<FichaEvaluacion> lista_fichas;
+        private int cantidad_admitidos;
+        private int cantidad_postulantes;
+
+        public int Cantidad_admitidos { get => cantidad_admitidos; set => cantidad_admitidos = value; }
+        public int Cantidad_postulantes { get => cantidad_postulantes; set => cantidad_postulantes = value; }
+        internal List<FichaEvaluacion> Lista_fichas { get => lista_fichas; }
+        public void agregarFichaDeEvaluacion(FichaEvaluacion ficha)
+        {
+            lista_fichas.Add(ficha);
+            cantidad_postulantes++;
+
+            if(ficha.Estado == FichaEvaluacion.estado_candidato.ADMITIDO)
+            {
+                cantidad_admitidos++;
+            }
+        }
+    }
+}
